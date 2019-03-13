577b6b108993b883f1b644aa624695878aa404d8
Paul Tonning <paul.tonning@student.vvc.edu>
2019-03-12 18:51:42 -0700

    update

--- loops/while/main.py
+++ loops/while/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import random
 
 
 # demonstrate the while loop
-count = 10
-while count > 0:
-   print("{}".format(count) )
-   count = count - 1
+#            0 1 2 3 4 5 
+diecounts = [0,0,0,0,0,0]
+count = 0
+while count < 100000:
+   r = random.randint(1,6)
 
+   diecounts[r-1] += 1
+   count += 1
 
-
-
+print(diecounts)
