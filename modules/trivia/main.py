b928f892257b0741d8e82f667a89a640ae26116c
Paul Tonning <paul.tonning@student.vvc.edu>
2019-05-21 18:32:48 -0700

    update

--- modules/trivia/main.py
+++ modules/trivia/main.py
@@ -1,26 +1,41 @@
-#from  trivia import Question
-#from  trivia import Player
+from  trivia import Question
+from  trivia import Player
 
-class Question:
-   
-   def __init__(self,text,a1,a2,a3,a4,answer):
-      self.text = text
-      self.a1 = a1
-      self.a2 = a2
-      self.a3 = a3
-      self.a4 = a4
-      self.answer = answer
 
-   def __str__(self):
-      return self.text + "\n" + self.a1 + "\n" + self.a2 + "\n" + self.a3 + \
-      "\n" + self.a4 + "\n"
-
-questions = [Question("In the human body, what is the hallux?","Head","Nose","Foot","Big Toe",4)
-
+questions = [
+   Question("Question 0 text","q0 option 1","q0 option 2","q0 option 3","q0 option 4",1),
+   Question("Question 1 text","q1 option 1","q1 option 2","q1 option 3","q1 option 4",2),
+   Question("Question 2 text","q2 option 1","q2 option 2","q2 option 3","q2 option 4",3),
+   Question("Question 3 text","q3 option 1","option 2","option 3","q3 option 4",0),
+   Question("Question 4 text","option 1","option 2","option 3","option 4",1),
+   Question("Question 5 text","option 1","option 2","option 3","option 4",2),
+   Question("Question 6 text","option 1","option 2","option 3","option 4",3),
+   Question("Question 7 text","option 1","option 2","option 3","option 4",4),
+   Question("Question 8 text","option 1","option 2","option 3","option 4",0),
+   Question("Question 9 text","option 1","option 2","option 3","option 4",1)
 ]
 
-#player1 = Player("Paul")
-#player2 = Player("Zach")
+player1 = Player("Jack")
+player2 = Player("Jill")
 
+# do player 1's questions
 for question in range(5):
-   print(questions[question])
+# display the question
+# get answer from player 1
+# check the answer using Questions checkAnswer method
+#   if the answer is correct
+#     player1 gets a point 
+
+for question in range(5,10):
+# display the question
+# get answer from player 2
+# check the answer using Questions checkAnswer method
+#   if the answer is correct
+#     player1 gets a point 
+
+if player1.points > player2.points:
+# player1 wins
+elif player1.points < player2.points:
+# player2 wins
+else
+# tie
